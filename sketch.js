fd6d32f6d0cdb311dcb46438ae0a9a62cdbea587
Yukeshwar <123797754+Yukeshwar@users.noreply.github.com>
2024-10-02 23:43:07 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,17 @@
+//for the second one i used Yayoi Kusama (Dots Obsession)
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(4000, 4000); 
+  background(255, 204, 0); 
+  fill(0); 
+  ellipse(50, 50, 60, 60);  
+  ellipse(150, 100, 80, 80);
+  ellipse(250, 50, 70, 70);
+  ellipse(350, 150, 100, 100);
+  ellipse(100, 250, 90, 90);
+  ellipse(200, 300, 80, 80);
+  ellipse(300, 250, 110, 110);
+  ellipse(50, 350, 70, 70);
+  ellipse(350, 350, 60, 60);
 }
 
 function draw() {
